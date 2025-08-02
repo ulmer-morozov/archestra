971d7ea66f849791f2e7c35451a00c8a491c557a
Ildar Iskhakov <ildar6v@gmail.com>
2025-08-02 16:32:08 +0100

    Last change

--- desktop_app/src/renderer.tsx
+++ desktop_app/src/renderer.tsx
@@ -1,7 +1,7 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import App from './App';
 
 console.log('👋 This message is being logged by "renderer.ts", included via Vite');
 
