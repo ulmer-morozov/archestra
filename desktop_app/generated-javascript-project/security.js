fb562e3bb741c7366365717119a5d264941ff9f8
Joey Orlando <joseph.t.orlando@gmail.com>
2025-08-04 20:33:35 -0400

    chore: remove `generate-javascript-project`

--- desktop_app/generated-javascript-project/security.js
+++
@@ -1,9 +0,0 @@
-// implementation of the security schemes in the openapi specification
-
-export class Security {
-	async initialize(schemes) {
-		// schemes will contain securitySchemes as found in the openapi specification
-		console.log("Initialize:", JSON.stringify(schemes));
-	}
-
-}
