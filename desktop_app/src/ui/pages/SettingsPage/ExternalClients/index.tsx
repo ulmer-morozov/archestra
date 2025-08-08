dd0c7e0da04fdb3173864e32ff27a91cc9c1e612
Joey Orlando <joseph.t.orlando@gmail.com>
2025-08-07 21:15:11 -0400

    chore: fix type error in `ExternalClients` page component

--- desktop_app/src/ui/pages/SettingsPage/ExternalClients/index.tsx
+++ desktop_app/src/ui/pages/SettingsPage/ExternalClients/index.tsx
@@ -19,7 +19,7 @@ export default function ExternalClients() {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {supportedExternalMcpClientNames.map((clientName) => {
         const connectedExternalMcpClient = connectedExternalMcpClients.find(
-          (client) => client.client_name === clientName
+          (client) => client.clientName === clientName
         );
         const isConnected = connectedExternalMcpClient !== undefined;
 
